16d052997d36dddbedb04f836bd48f6c011429cf
Yuliana <yulianamarcela200@gmail.com>
2021-08-03 19:46:09 -0400

    Ya rebota la pelota

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,31 +3,27 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import Objects.Ball;
+
 import com.badlogic.gdx.graphics.GL20;
 
 public class MyGdxGame extends ApplicationAdapter {
     ShapeRenderer shape;
-    int x = 50;
-    int y = 50;
-    int xSpeed = 5;
+    Ball ball;
 
     @Override
     public void create () {
         shape = new ShapeRenderer();
+        ball = new Ball(150, 200, 70, 12, 5);
     }
 
     @Override
     public void render() {
-    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); 
-    	 x += xSpeed;
-    	    if (x > Gdx.graphics.getWidth()) {
-    	        xSpeed = -5;
-    	    }
-    	    if (x < 0) {
-    	        xSpeed = 5;
-    	    }
+    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        ball.update();
         shape.begin(ShapeRenderer.ShapeType.Filled);
-        shape.circle(x, y, 50);
+        ball.draw(shape);
         shape.end();
     }
 }
